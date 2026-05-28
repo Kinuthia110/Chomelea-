1925af5f99742e6060921b0872a7f4be42385e65
Lenny <kitengela629@gmail.com>
2026-05-28 10:10:13 +0300

    Enable Apollo Sandbox

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,73 @@
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import app from "./app.js";
+import path from "path";
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+
 import connectDB from "./config/db.js";
+
+import typeDefs from "./graphql/typeDefs/index.js";
+import resolvers from "./graphql/resolvers/index.js";
+
+import uploadRoutes from "./routes/uploadRoutes.js";
+import mpesaRoutes from "./routes/mpesaRoutes.js";
 
 dotenv.config();
 
-connectDB();
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+await connectDB();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true
+  })
+);
+
+app.use(express.json());
+
+app.use(
+  "/quotations",
+  express.static(path.join(process.cwd(), "src/invoices/generated"))
+);
+
+app.use(
+  "/invoices",
+  express.static(path.join(process.cwd(), "src/invoices/generated"))
+);
+
+app.use("/api/uploads", uploadRoutes);
+app.use("/api/mpesa", mpesaRoutes);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({
+      embed: true
+    })
+  ]
+});
+
+await server.start();
+
+app.use(
+  "/graphql",
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req })
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("CHOMELEA Backend Running");
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
