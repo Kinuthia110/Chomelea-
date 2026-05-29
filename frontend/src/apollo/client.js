e74c0594e05baa94da9a6f28ff8056abc32cfd06
Lenny <kitengela629@gmail.com>
2026-05-29 14:22:58 +0300

    Send auth token in Apollo requests

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -1,12 +1,29 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_API_URL,
+  uri: import.meta.env.VITE_API_URL
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
 });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 export default client;
