eb3aeda01c8ab76ce18888faf743f7adf1dea694
Lenny <kitengela629@gmail.com>
2026-05-29 14:35:07 +0300

    Fix authorization header

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -13,10 +13,12 @@ const httpLink = createHttpLink({
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token");
 
+  console.log("TOKEN SENT:", token);
+
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ""
+      Authorization: token ? `Bearer ${token}` : ""
     }
   };
 });
