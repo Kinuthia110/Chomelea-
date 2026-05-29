faf6b99c21b9e0ad4c0423854e1699a7c3267784
Lenny <kitengela629@gmail.com>
2026-05-29 11:15:14 +0300

    Protect env files and use production API URL

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -1,29 +1,12 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink
-} from "@apollo/client";
-
-import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_API_URL
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("token");
-
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ""
-    }
-  };
+  uri: import.meta.env.VITE_API_URL,
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  link: httpLink,
+  cache: new InMemoryCache(),
 });
 
 export default client;
